a72e0bec0eecfa7c1b836e1d728e06b627e9f9d3
trinset <o_chigu@mail.ru>
2019-11-05 19:38:30 +0300

    Add files via upload

---
+++ C++.Workshop1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "my_sum.h"
+
+using namespace std;
+int main ()
+{
+	int a;
+	int b;
+
+	cin >> a >> b;
+	my_sum(a, b);
+}
---
+++ C++.Workshop1/my_sum.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int my_sum (int a, int b)
+{
+	std::cout << a + b << std::endl;
+}
